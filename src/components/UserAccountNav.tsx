c5e89d295651b5582f19b686c5081c6e7a6e9abf
sidxh <totallysiddhant@gmail.com>
2023-12-29 04:08:35 +0530

    UserAccountNav.tsx fixes

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,37 +1,60 @@
-import { getUserSubscriptionPlan } from '@/lib/stripe'
+import React, { useEffect, useState } from 'react';
+import { getUserSubscriptionPlan } from '@/lib/stripe';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from './ui/dropdown-menu'
-import { Button } from './ui/button'
-import { Avatar, AvatarFallback } from './ui/avatar'
-import Image from 'next/image'
-import { Icons } from './Icons'
-import Link from 'next/link'
-import { Gem } from 'lucide-react'
-import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs/server'
+} from './ui/dropdown-menu';
+import { Button } from './ui/button';
+import { Avatar, AvatarFallback } from './ui/avatar';
+import Image from 'next/image';
+import { Icons } from './Icons';
+import Link from 'next/link';
+import { Gem } from 'lucide-react';
+import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs/server';
 
 interface UserAccountNavProps {
-  email: string | undefined
-  name: string
-  imageUrl: string
+  email: string | undefined;
+  name: string;
+  imageUrl: string;
 }
 
-const UserAccountNav = async ({
-  email,
-  imageUrl,
-  name,
-}: UserAccountNavProps) => {
-  const subscriptionPlan = await getUserSubscriptionPlan()
+interface SubscriptionPlan {
+  isSubscribed: boolean;
+  isCanceled: boolean;
+  stripeCurrentPeriodEnd: null;
+  name: string;
+  slug: string;
+  quota: number;
+  pagesPerPdf: number;
+  price: {
+    amount: number;
+    priceIds: {
+      test: string;
+      production: string;
+    };
+  };
+}
+
+const UserAccountNav = ({ email, imageUrl, name }: UserAccountNavProps) => {
+  
+const [subscriptionPlan, setSubscriptionPlan] = useState<SubscriptionPlan | null>(null);
+
+useEffect(() => {
+    const fetchSubscriptionPlan = async () => {
+      const plan = await getUserSubscriptionPlan();
+      const [subscriptionPlan, setSubscriptionPlan] = useState<{ isSubscribed: boolean; isCanceled: boolean; stripeCurrentPeriodEnd: null; name: string; slug: string; quota: number; pagesPerPdf: number; price: { amount: number; priceIds: { test: string; production: string; }; }; } | null>(null);
+
+    };
+
+    fetchSubscriptionPlan();
+}, []);
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger
-        asChild
-        className='overflow-visible'>
+      <DropdownMenuTrigger asChild className='overflow-visible'>
         <Button className='rounded-full h-8 w-8 aspect-square bg-slate-400'>
           <Avatar className='relative w-8 h-8'>
             {imageUrl ? (
@@ -56,11 +79,7 @@ const UserAccountNav = async ({
       <DropdownMenuContent className='bg-white' align='end'>
         <div className='flex items-center justify-start gap-2 p-2'>
           <div className='flex flex-col space-y-0.5 leading-none'>
-            {name && (
-              <p className='font-medium text-sm text-black'>
-                {name}
-              </p>
-            )}
+            {name && <p className='font-medium text-sm text-black'>{name}</p>}
             {email && (
               <p className='w-[200px] truncate text-xs text-zinc-700'>
                 {email}
@@ -77,13 +96,10 @@ const UserAccountNav = async ({
 
         <DropdownMenuItem asChild>
           {subscriptionPlan?.isSubscribed ? (
-            <Link href='/dashboard/billing'>
-              Manage Subscription
-            </Link>
+            <Link href='/dashboard/billing'>Manage Subscription</Link>
           ) : (
             <Link href='/pricing'>
-              Upgrade{' '}
-              <Gem className='text-blue-600 h-4 w-4 ml-1.5' />
+              Upgrade <Gem className='text-blue-600 h-4 w-4 ml-1.5' />
             </Link>
           )}
         </DropdownMenuItem>
@@ -91,11 +107,11 @@ const UserAccountNav = async ({
         <DropdownMenuSeparator />
 
         <DropdownMenuItem className='cursor-pointer'>
-            <LogoutLink>Log out</LogoutLink>
+          <LogoutLink>Log out</LogoutLink>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
-}
+  );
+};
 
-export default UserAccountNav
+export default UserAccountNav;
