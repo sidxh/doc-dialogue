eb57d5317f51d9cfce5f254115178df6e019b191
sidxh <totallysiddhant@gmail.com>
2023-12-28 20:16:53 +0530

    committed components

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,49 +1,59 @@
-"use client"
+'use client'
 
-import UploadButton from "./UploadButton";
-import { trpc } from "@/app/_trpc/client";
-import Skeleton from "react-loading-skeleton";
-import {format} from "date-fns"
+import { trpc } from '@/app/_trpc/client'
+import UploadButton from './UploadButton'
 import {
-    Ghost,
-    Loader2,
-    MessageSquare,
-    Plus,
-    Trash,
-  } from 'lucide-react'
-  import Link from 'next/link'
-  import { Button } from './ui/button'
-  import { useState } from 'react'
+  Ghost,
+  Loader2,
+  MessageSquare,
+  Plus,
+  Trash,
+} from 'lucide-react'
+import Skeleton from 'react-loading-skeleton'
+import Link from 'next/link'
+import { format } from 'date-fns'
+import { Button } from './ui/button'
+import { useState } from 'react'
+import { getUserSubscriptionPlan } from '@/lib/stripe'
 
-const Dashboard = () => {
-    const [currentlyDeletingFile, setCurrentlyDeletingFile] =
+interface PageProps {
+  subscriptionPlan: Awaited<ReturnType<typeof getUserSubscriptionPlan>>
+}
+
+const Dashboard = ({subscriptionPlan}: PageProps) => {
+  const [currentlyDeletingFile, setCurrentlyDeletingFile] =
     useState<string | null>(null)
 
-    const utils = trpc.useContext()
+  const utils = trpc.useContext()
 
-    const { data: files, isLoading } =
-        trpc.getUserFiles.useQuery()
+  const { data: files, isLoading } =
+    trpc.getUserFiles.useQuery()
 
-    const { mutate: deleteFile } =
-        trpc.deleteFile.useMutation({
-        onSuccess: () => {
-            utils.getUserFiles.invalidate()
-        },
-        onMutate({ id }) {
-            setCurrentlyDeletingFile(id)
-        },
-        onSettled() {
-            setCurrentlyDeletingFile(null)
-        },
+  const { mutate: deleteFile } =
+    trpc.deleteFile.useMutation({
+      onSuccess: () => {
+        utils.getUserFiles.invalidate()
+      },
+      onMutate({ id }) {
+        setCurrentlyDeletingFile(id)
+      },
+      onSettled() {
+        setCurrentlyDeletingFile(null)
+      },
     })
-    return(
-        <main className="mx-auto max-w-7xl md:p-10">
-            <div className="mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0">
-                <h1 className='mb-3 font-bold text-5xl text-gray-900'>My Files</h1>
-                <UploadButton />
-            </div>
 
-            {files && files?.length !== 0 ? (
+  return (
+    <main className='mx-auto max-w-7xl md:p-10'>
+      <div className='mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0'>
+        <h1 className='mb-3 font-bold text-5xl text-gray-900'>
+          My Files
+        </h1>
+
+        <UploadButton isSubscribed={subscriptionPlan.isSubscribed} />
+      </div>
+
+      {/* display all user files */}
+      {files && files?.length !== 0 ? (
         <ul className='mt-8 grid grid-cols-1 gap-6 divide-y divide-zinc-200 md:grid-cols-2 lg:grid-cols-3'>
           {files
             .sort(
@@ -112,9 +122,8 @@ const Dashboard = () => {
           <p>Let&apos;s upload your first PDF.</p>
         </div>
       )}
-            
-        </main>
-    )
+    </main>
+  )
 }
 
-export default Dashboard;
+export default Dashboard
