fcf6f15c28289b0c27a1021ea780ce0c75dfe74d
sidxh <totallysiddhant@gmail.com>
2023-12-29 12:42:11 +0530

    utils paths

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,12 +7,10 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function absoluteUrl(path: string) {
-  if (typeof window !== 'undefined') return path
-  if (process.env.VERCEL_URL)
-    return `https://${process.env.VERCEL_URL}${path}`
-  return `http://localhost:${
-    process.env.PORT ?? 3000
-  }${path}`
+  const baseUrl = process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : `http://localhost:${process.env.PORT ?? 3000}`;
+  return `${baseUrl}${path}`;
 }
 
 export function constructMetadata({
